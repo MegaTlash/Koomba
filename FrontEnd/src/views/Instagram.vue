b961367b4ca95610ee23622e29ddaf8ad2efa1f8
JaG31 <James.Gillies@ontariotechu.net>
2020-03-22 17:27:32 -0400

    Added instagram tab functionality (with images and links)

--- FrontEnd/src/views/Instagram.vue
+++ FrontEnd/src/views/Instagram.vue
@@ -1,6 +1,23 @@
 <template>
     <div class = "Instagram">
-        <p1>HELLO INSTAGRAM</p1>
+        <div class="link">
+            <a href=https://instagram.com/_koomba_><img src="../assets/instagram.jpg" height=100 width=100></a>
+        </div>
+
+    <div class="Photos">
+        <div class="firstPhoto">
+            <iframe id="instagram-embed-0" class="instagram-media instagram-media-rendered" style="background: white; max-width: 658px; width: calc(100% - 2px); border-radius: 3px; border: 1px solid #dbdbdb; box-shadow: none; display: block; margin: 0px 0px 12px; min-width: 326px; padding: 0px;" src="https://www.instagram.com/p/BxGUzIVpSk3/embed?utm_source=ig_embedembed/captioned/" scrolling="no" data-instgrm-payload-id="instagram-media-payload-0" height="700px" width="150" frameborder="0"><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span></iframe>
+        </div>
+
+        <div class="secondPhoto">
+            <iframe id="instagram-embed-0" class="instagram-media instagram-media-rendered" style="background: white; max-width: 658px; width: calc(100% - 2px); border-radius: 3px; border: 1px solid #dbdbdb; box-shadow: none; display: block; margin: 0px 0px 12px; min-width: 326px; padding: 0px;" src="https://www.instagram.com/p/BuCyYJ8Fz06/embed?utm_source=ig_embedembed/captioned/" scrolling="no" data-instgrm-payload-id="instagram-media-payload-0" height="700" width="150" frameborder="0"><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span></iframe>
+        </div>
+
+        <div class="thirdPhoto">
+            <iframe id="instagram-embed-0" class="instagram-media instagram-media-rendered" style="background: white; max-width: 658px; width: calc(100% - 2px); border-radius: 3px; border: 1px solid #dbdbdb; box-shadow: none; display: block; margin: 0px 0px 12px; min-width: 326px; padding: 0px;" src="https://www.instagram.com/p/B7KnatKJSzn/embed?utm_source=ig_embedembed/captioned/" scrolling="no" data-instgrm-payload-id="instagram-media-payload-0" height="700" width="150" frameborder="0"><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"></span></iframe>
+        </div>
+
+    </div>
     </div>
 </template>
 
@@ -11,3 +28,36 @@ export default {
     name: 'Instagram',
 };
 </script>
+
+
+<style scoped>
+.link {
+    
+}
+
+.Photos {
+    margin: auto;
+    text-align:center;
+}
+
+.firstPhoto {
+    display: inline-block;
+    width: 30%;
+    padding: 2rem;
+    margin: auto;
+}
+
+.secondPhoto {
+    display: inline-block;
+    width: 30%;
+    padding: 2rem;
+    margin: auto;
+}
+
+.thirdPhoto {
+    display: inline-block;
+    width: 30%;
+    padding: 2rem;
+    margin: auto;
+}
+</style>
