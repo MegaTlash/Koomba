c12505f75e1366c514b4ee27c498370373bb87fa
Tamilesh <tamilesh123@gmail.com>
2020-04-09 00:57:05 -0400

    Cloud Connector maybe might use

--- BackEnd/routes/api/clothes.js
+++ BackEnd/routes/api/clothes.js
@@ -1,13 +1,25 @@
 const express = require('express');
-//const mongodb = require('mongodb');
+const mongodb = require('mongodb');
 
 const router = express.Router();
 
+/*
+
 //Get Posts
-router.get('/', (req, res) => {
-    res.send('hello');
+router.get('/', async (req, res) => {
+    const posts = await loadPostCollection();
+    res.send(await posts.find({}).toArray());
+
 })
 
 
+//loadPostsCollection
+async function loadPostCollection(){
+        useNewUrlParser: true
+    })
+    return client.db('vue_express').collection('posts');
+}
+
+*/
 
 module.exports = router;
---
+++ FrontEnd/src/components/ShoppingCart.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class = "navbar">
+            
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
