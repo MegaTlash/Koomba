35384aec0e8bd9924a48886746c173df6323559f
Kim Montemayor <kim.montemayor@uoit.net>
2020-04-10 00:34:33 -0400

    error checking register (almost complete)

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -8,12 +8,40 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+//Registering a new user
 app.post('/signup', (req, res, next) =>{
 
     firstName = req.body.firstName;
     lastName = req.body.lastName;
     email = req.body.email;
     password = req.body.password;
+
+    var errorFN = '', errorLN = '', errorEmail = '', errorPass = '';
+    var isErrFN = false, isErrLN = false, isErrEmail = false, isErrPass = false;
+
+    if (firstName == ''){
+        errorFN = 'First name cannot be empty';
+        isErrFN = true;
+    }
+
+    if (lastName == ""){
+        errorLN = 'Last name cannot be empty';
+        isErrLN = true;
+    }
+
+    if (email == ''){
+        errorEmail = 'E-mail cannot be empty';
+        isErrEmail = true;
+    }
+
+    if (password == ''){
+        errorPass = 'Password cannot be empty';
+        isErrPass = true;
+    }
+    else if (password.length < 6){
+        errorPass = 'Password must be at least 6 characters';
+        isErrPass = true;
+    }
 
     hashedPassword = bcrypt.hashSync(password, 10);
 
@@ -23,11 +51,51 @@ app.post('/signup', (req, res, next) =>{
         email: email,
         hashedPassword: hashedPassword
     });
-    console.log(newUser);
-    newUser.save(function(error) {
-        if (error) {
-           console.log('error')
+    
+    //Catching errors (might be the problem why valid email is getting pushed with empty inputs)
+    if(isErrFN && isErrLN && isErrEmail && isErrPass){
+        return res.json({
+            title: 'errors',
+
+            errorFN: errorFN,
+            errorLN: errorLN,
+            errorEmail: errorEmail,
+            errorPass: errorPass,
+            
+            isErrFN: isErrFN,
+            isErrLN: isErrLN,
+            isErrEmail: isErrEmail,
+            isErrPass: isErrPass
+        })
+    }
+
+    newUser.save(err => {
+
+        //E-mail already exists in database
+        if (err) {
+            console.log('e')
+            return res.json({
+                title: 'emailExists',
+                
+                errorFN: errorFN,
+                errorLN: errorLN,
+                errorEmail: 'Email already exists',
+                errorPass: errorPass,
+            
+                isErrFN: isErrFN,
+                isErrLN: isErrLN,
+                isErrPass: isErrPass
+            })
         }
+
+        //Returning inputs, however, there may still be errors
+        return res.json({
+            
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+        })
     });
 });
 
--- FrontEnd/src/views/SignUp.vue
+++ FrontEnd/src/views/SignUp.vue
@@ -6,39 +6,64 @@
             crossorigin="anonymous">
         <h1>SIGN UP</h1>
          <div class="form">
+
+             <!-- First Name Input -->
             <div class="field">
                 <p class="control has-icons-left has-icons-right">
-                    <input class="input" type="text" v-model="firstName" placeholder="First Name">
+                    <input :class="inputType1" type="text" v-model="firstName" placeholder="First Name">
                     <span class="icon is-small is-left">
                         <i class="fas fa-user"></i>
                     </span>
+                    <span class="icon is-small is-right">
+                        <i :class="icon1"></i>
+                    </span>
                 </p>
+                <p :class="pClass1">{{errMsg1}}</p>
             </div>
+             
+            <!-- Last Name Input -->
             <div class="field">
                 <p class="control has-icons-left has-icons-right">
-                    <input class="input" type="text" v-model="lastName" placeholder="Last Name">
+                    <input :class="inputType2" type="text" v-model="lastName" placeholder="Last Name">
                     <span class="icon is-small is-left">
                         <i class="fas fa-id-card"></i>
                     </span>
+                    <span class="icon is-small is-right">
+                        <i :class="icon2"></i>
+                    </span>
                 </p>
+                <p :class="pClass2">{{errMsg2}}</p>
             </div>
+
+            <!-- Email Input -->
             <div class="field">
                 <p class="control has-icons-left has-icons-right">
-                    <input class="input" type="email" v-model="email" placeholder="Email">
+                    <input :class="inputType3" type="email" v-model="email" placeholder="Email">
                     <span class="icon is-small is-left">
                         <i class="fas fa-envelope"></i>
                     </span>
+                    <span class="icon is-small is-right">
+                        <i :class="icon3"></i>
+                    </span>
                 </p>
+                <p :class="pClass3">{{errMsg3}}</p>
             </div>
+
+            <!-- Email Input -->
             <div class="field">
-            <p class="control has-icons-left">
-                <input class="input" type="password" v-model="password" placeholder="Password">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-lock"></i>
-                </span>
-            </p>
+                <p class="control has-icons-left has-icons-right">
+                    <input :class="inputType4" type="password" v-model="password" placeholder="Password">
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-envelope"></i>
+                    </span>
+                    <span class="icon is-small is-right">
+                        <i :class="icon4"></i>
+                    </span>
+                </p>
+                <p :class="pClass4">{{errMsg4}}</p>
             </div>
         </div>
+        
         <button @click="register" class="button is-success">Register</button>
     </div>
 </template>
@@ -52,7 +77,27 @@ export default {
             firstName: '',
             lastName: '',
             email: '',
-            password: ''
+            password: '',
+
+            pClass1: '',
+            pClass2: '',
+            pClass3: '',
+            pClass4: '',
+
+            inputType1: 'input is-dark',
+            inputType2: 'input is-dark',
+            inputType3: 'input is-dark',
+            inputType4: 'input is-dark',
+            
+            errMsg1: '',
+            errMsg2: '',
+            errMsg3: '',
+            errMsg4: '',
+
+            icon1: '',
+            icon2: '',
+            icon3: '',
+            icon4: '',
         }
     },
     methods: {
@@ -64,7 +109,76 @@ export default {
                password: this.password
            }
             //console.log(newUser);
-            axios.post('http://localhost:3000/signup', newUser);
+            axios.post('http://localhost:3000/signup', newUser)
+                .then(res => {
+                    //console.log(res.data);
+
+                    if (res.data.title == 'errors' || res.data.title == 'emailExists'){
+                        
+                        //First name input is empty
+                        if(res.data.isErrFN){
+                            this.inputType1 = 'input is-danger'
+                            this.pClass1 = 'help is-danger';
+                            this.errMsg1 = res.data.errorFN;
+                            this.icon1 = 'fas fa-exclamation-triangle'
+                        }
+                        else{
+                            this.inputType1 = 'input is-success'
+                            this.pClass1 = '';
+                            this.errMsg1 = '';
+                            this.icon1 = 'fas fa-check'
+                        }
+
+                        //Last name input is empty
+                        if(res.data.isErrLN){
+                            this.inputType2 = 'input is-danger'
+                            this.pClass2 = 'help is-danger';
+                            this.errMsg2 = res.data.errorLN;
+                            this.icon2 = 'fas fa-exclamation-triangle'
+                        }
+                        else{
+                            this.inputType2 = 'input is-success'
+                            this.pClass2 = '';
+                            this.errMsg2 = '';
+                            this.icon2 = 'fas fa-check'
+                        }
+
+                        //Email input is empty or already in use
+                        if(res.data.isErrEmail || res.data.title == 'emailExists'){
+                            this.inputType3 = 'input is-danger'
+                            this.pClass3 = 'help is-danger';
+                            this.errMsg3 = res.data.errorEmail;
+                            this.icon3 = 'fas fa-exclamation-triangle'
+                        }
+                        else{
+                            this.inputType3 = 'input is-success'
+                            this.pClass3 = '';
+                            this.errMsg3 = '';
+                            this.icon3 = 'fas fa-check'
+                        }
+
+                        //Password input is empty or input is less than 6 characters
+                        if(res.data.isErrPass){
+                            this.inputType4 = 'input is-danger'
+                            this.pClass4 = 'help is-danger';
+                            this.errMsg4 = res.data.errorPass;
+                            this.icon4 = 'fas fa-exclamation-triangle'
+                        }
+                        else{
+                            this.inputType4 = 'input is-success'
+                            this.pClass4 = '';
+                            this.errMsg4 = '';
+                            this.icon4 = 'fas fa-check'
+                        }
+                    }
+                    else{
+                        console.log('success')
+                    }
+
+                    
+                    //E-mail already in use
+                    
+                })
         }
     }
 };
@@ -85,6 +199,10 @@ h1 {
 
 .form{
     padding: 2rem 25rem 2rem 25rem;
+}
+
+p {
+    text-align: left;
 }
 
 </style>
