3a1c2155b3fbfbe9d49075fdff176dec2114422a
JaG31 <James.Gillies@ontariotechu.net>
2020-04-11 21:19:21 -0400

    Starting the storepage

--- FrontEnd/src/views/Store.vue
+++ FrontEnd/src/views/Store.vue
@@ -1,17 +1,69 @@
+<!--
+<template>
+  <div id="Store">
+    <ul>
+        <Items v-bind:items="items"/>
+        <li v-for="item in items" :key="item">
+          {{item.itemType}}
+        </li>
+    </ul>
+ </div>   
+</template>
+
+
+<script>
+export default {
+    name: 'Store',
+    data() {
+      return {
+        items: [
+          {
+            _id: "001",
+            itemType: "T-Shirt",
+            itemPrice: "25",
+            itemColour: "Blue",
+            detailsURL: "/tbd?id=001",
+            imageURL: "tbd"
+          },
+
+          {
+            _id: "002",
+            itemType: "Sweater",
+            itemPrice: "60",
+            itemColour: "Black",
+            detailsURL: "/tbd?id=001",
+            imageURL: "tbd"
+          }
+        ]
+      }
+      
+    }
+};
+
+
+
+
+</script>
+
+<style scoped>
+
+</style>
+-->
+
 <template>
     <div class = "Store">
-        <table id="items">
-            <img src=../assets/Models/Jordan/JordanOutsidePicRain.jpg width=200>
-        </table>
-        <h1>Store</h1>
+         <ul>
+        <!--<Items v-bind:items="items"/>-->
+        <li v-for="(item, idx) in items" :key="idx">
+          
+          <img :src="item.imageURL"/>
+          <a1>{{item.itemType}}</a1>
+          <a2>{{item.itemPrice}}</a2>
+          
+        </li>
+        </ul>
         <div class = "clothes-container">
-            <div class = "clothes"
-                v-for="{item, index} in clothes"
-                v-bind:item="item"
-                v-bind:index="index"
-                v-bind:key ="item._id"
-            >
-            <p class = "text">{{item.itemType}}</p>
+            <div class = "clothes">
             </div>
         </div>
     </div>
@@ -20,31 +72,84 @@
 
 
 <script>
-import ClothesService from '../ClothesServices'
+//import ClothesService from '../ClothesServices';
 export default {
     name: 'Store',
-    mounted : function() {
-        var items = document.getElementById("items");
-        items.innerHTML = '<img src="../assets/Models/Jordan/JordanOutsidePicRain.jpg" width=200>';
-        console.log(items.innerHTML);
-    },
-    
-
     data(){
         return {
-            clothes: [],
+            items: [{
+            _id: "001",
+            itemType: "T-Shirt",
+            itemPrice: "25",
+            itemColour: "Blue",
+            detailsURL: "/tbd?id=001",
+            imageURL: require('../assets/Models/Jordan/JordanOutsidePicRain.jpg')
+          },
+
+          {
+            _id: "002",
+            itemType: "Sweater",
+            itemPrice: "60",
+            itemColour: "Black",
+            detailsURL: "/tbd?id=002",
+            imageURL: require('../assets/Models/Jordan/JordanOutsidePicRain.jpg')
+          },
+
+          {
+            _id: "002",
+            itemType: "Sweater",
+            itemPrice: "60",
+            itemColour: "Black",
+            detailsURL: "/tbd?id=002",
+            imageURL: require('../assets/Models/Jordan/JordanOutsidePicRain.jpg')
+          },
+
+          {
+            _id: "002",
+            itemType: "Sweater",
+            itemPrice: "60",
+            itemColour: "Black",
+            detailsURL: "/tbd?id=002",
+            imageURL: require('../assets/Models/Jordan/JordanOutsidePicRain.jpg')
+          },
+
+          {
+            _id: "002",
+            itemType: "Sweater",
+            itemPrice: "60",
+            itemColour: "Black",
+            detailsURL: "/tbd?id=002",
+            imageURL: require('../assets/Models/Jordan/JordanOutsidePicRain.jpg')
+          },
+
+          {
+            _id: "002",
+            itemType: "Sweater",
+            itemPrice: "60",
+            itemColour: "Black",
+            detailsURL: "/tbd?id=002",
+            imageURL: require('../assets/Models/Jordan/JordanOutsidePicRain.jpg')
+          }
+
+
+          ],
             error: '',
             text: '',
         }
     },
-    async created(){
+    methods: {
+      getImg(imgName) {
+        return require(imgName)
+      }
+    }
+    /*async created(){
         try{
             this.clothes = ClothesService.getClothes();
             console.log(this.clothes);
         }catch(err){
             this.error = err.message;
         }
-    }
+    }*/
 };
 
 
@@ -56,4 +161,19 @@ h1 {
     font-size:3rem;
     font-weight: bold;
 }
+
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  padding-left: 20rem;
+  padding-right: 20rem;
+}
+
+ul li {
+    list-style: none;
+    flex-basis: 33.333333%;
+    padding: 3rem;
+}
+
 </style>
+
