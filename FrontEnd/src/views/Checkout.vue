9eb04e80c267d0c243f64eb3a5956a6174589fd2
JaG31 <James.Gillies@ontariotechu.net>
2020-04-14 23:07:46 -0400

    Removed errors

--- FrontEnd/src/views/Checkout.vue
+++ FrontEnd/src/views/Checkout.vue
@@ -1,18 +1,13 @@
 <template>
     <div class="Checkout">
-        <li v-for="item in items" :key="item.name">
-            {{item.name}}
-        </li>
+        
     </div>
 </template>
 
 <script>
-import items from 'ShoppingCart.vue'
 export default {
     methods: { 
-        getItems() {
-            return items;
-        }
+
     }
 }
 
